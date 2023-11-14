c692eb8ef6a7c89ac9e1e917ed6f5e094117acb3
Your Name <you@example.com>
2023-11-14 22:54:23 +0200

    Do not redirect 404 error on proxying endpoins

--- django_vpn_service/django_vpn_service/middleware.py
+++ django_vpn_service/django_vpn_service/middleware.py
@@ -1,5 +1,6 @@
-from django.shortcuts import redirect
 from django.middleware.common import CommonMiddleware
+from django.shortcuts import redirect
+from django.urls import resolve
 
 
 class Redirect404Middleware(CommonMiddleware):
@@ -7,9 +8,21 @@ class Redirect404Middleware(CommonMiddleware):
         self.get_response = get_response
 
     def __call__(self, request):
+        requested_url = request.path
+        allow_404 = False
+
+        try:
+            match = resolve(requested_url)
+            url_name = match.url_name
+            # if it is endpoints for proxying, ignore 404 error and do not redirect
+            if url_name in ("proxy_data", "proxy_site"):
+                allow_404 = True
+        except Exception as e:
+            pass
+
         response = self.get_response(request)
         if self.should_redirect_with_slash(request):
             return response
-        if response.status_code == 404:
+        if response.status_code == 404 and not allow_404:
             return redirect("home")
         return response
