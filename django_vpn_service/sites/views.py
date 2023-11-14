b2c86968b5ea92c393e62ebad7428a2d0949868e
Your Name <you@example.com>
2023-11-14 14:56:10 +0200

    Fix link injection bug

--- django_vpn_service/sites/views.py
+++ django_vpn_service/sites/views.py
@@ -1,3 +1,5 @@
+from urllib.parse import urlparse
+
 from bs4 import BeautifulSoup
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
@@ -32,13 +34,17 @@ def home(request):
 
 def proxy_site(request, user_site_name, routes_on_original_site):
     get_object_or_404(Site, user=request.user, name=user_site_name)
+    parsed_original_site = urlparse(routes_on_original_site)
 
     response = requests.get(routes_on_original_site)
     content = response.content
 
     soup = BeautifulSoup(content, "html.parser")
     for link in soup.find_all("a", href=True):
-        if link['href'].startswith(routes_on_original_site):
+        parsed_href = urlparse(link["href"])
+        if (parsed_href.scheme == parsed_original_site.scheme
+            and parsed_href.netloc == parsed_original_site.netloc
+        ):
             link["href"] = f"/{user_site_name}/{link['href']}"
 
     return HttpResponse(str(soup))
